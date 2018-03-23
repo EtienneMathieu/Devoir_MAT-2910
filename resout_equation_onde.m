debc54c6fc41f90fdfc0ca1b526a2e80ba13d4ff
Geneviève Le Houx <gnvvlhx@gmail.com>
2018-03-23 15:04:16 -0400

    fichier resout_onde

--- resout_equation_onde.m
+++ resout_equation_onde.m
@@ -1,5 +1,16 @@
 function [u,erreur] = resout_equation_onde(c,Nt,Nx,theta,f,u0,u1tilde)
 %Cette fonction résout l'équation des ondes
 %   Detailed explanation goes here
+A = toeplitz([1+2*theta*c, -theta*c ,zeros(1, Nx-2)])
+%lien utile : https://math.stackexchange.com/questions/763909/creating-a-tridiagonal-matrix-in-matlab
+A(1,1) = 1
+for k = 2:Nx
+   A(1,k) = 0
+end
+A(Nx, Nx) = 1
+for k = 1:Nx-1
+   A(Nx,k) = 0
+end
+B0 = 
 end
 
