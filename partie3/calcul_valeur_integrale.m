3241a86b7b51e4c7cdbb73cfc4770b595644c2f7
SunFire <etienne.mathieu.2@ulaval.ca>
2018-04-06 15:03:02 -0400

    partie3/calcul_valeur_integrale.m

---
+++ partie3/calcul_valeur_integrale.m
@@ -0,0 +1,7 @@
+function [ J ] = calcul_valeur_integrale( u_c,u_ex )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
--- partie3/script_devoir_pb_inverse.m
+++ partie3/script_devoir_pb_inverse.m
@@ -2,7 +2,7 @@
 %que l’on va voir pour résoudre le problème inverse (sécante/différence finie et recuit
 %simulé) et permettant de trouver cexp. Dans toute la suite, pour toutes les méthodes, on
 %prendra les paramètres suivants:
-
+%Réutiliser la fonction résout_équation_ondes (Partie 2)
 global omega theta L T Nt Nx g f u0 u1tilde
 omega=5
 theta=0.5
@@ -15,6 +15,7 @@ f=@(t)cos(omegat)
 u0=@(x)0.1.*(1-x)
 u1tilde=@(x)0.1.*cos(omega.*deltat).*(1-x)
 
+%Méthode des trapèzes
 c=1
 while (c > 0) & (c < 100) %Intégrale de la fonction (11)
     
