52e5e43dc48efe5b2a4aa020eb42d150a90819e9
SunFire <etienne.mathieu.2@ulaval.ca>
2018-04-13 14:45:28 -0400

    ajout fichier recuit simule

--- partie3/pb_inv_recuit_simule.m
+++ partie3/pb_inv_recuit_simule.m
@@ -8,12 +8,27 @@ function [cfinal, iter] = pb_inv_recuit_simule(u_ex,cini,vois,nmax,f,u0,u1tilde)
 %- Résultat: une approximation x_global de la solution du problème:
 %min_x(f(x)) où x appartient aux réels.
 
+%a) Implémenter l’algorithme de recuit simulé présent dans l’annexe en question. La signature
+%de la fonction sera la suivante
+%function[cfinal,iter]=pb_inv_recuit_simule(u_ex,cini,vois,nmax,f,u0,u1tilde)
+%vois représente «la distance» à laquelle on va chercher un voisin (cf. algorithme), on
+%pourra prendre une valeur entre 15 et 20. Le nombre d’itérations est donné par nmax.
+%Pour les paramètres du recuit simulé définis dans l’annexe 3, on propose d’utiliser les
+%valeurs suivantes a = 0.8, R = 100, vois= 20 Lancer l’algorithme à partir des valeurs
+%de c suivantes c = 3; 15; 17; 21; 23. Que pouvez-vous dire quand à la précision et le coût
+%de cette méthode ?
+%Attention, comme mentionné dans l’annexe, s’agissant d’un algorithme stochastique,
+%pour chaque valeur de c on fera plusieurs lancements pour garantir le résultat !
+
+%b) Comment pourrait-on coupler les différentes méthodes afin d’assurer une convergence
+%vers cexp avec une bonne précision (en partant de n’importe quel cini)?
+
 k=0;
 y0=f(x0);
 x_global=x0; y_global=y0;
 n_max=1 % Nombre max d'itérations
 
-while R > 0.0001 & k <= n_max
+while (R > 0.0001 && k <= n_max)
     x1 = x0+v.*(random(0.1)-0.7);
     %calcul de y1 = f(x1);
     if y1 < y0
@@ -30,4 +45,4 @@ while R > 0.0001 & k <= n_max
     end
     k=k+1;
 end
-        
+ 
